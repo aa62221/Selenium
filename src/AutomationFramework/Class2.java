816bbe8c3ed32b5593f7ad19e6efdbfc5507db2f
aa62221 <akshayathale.m09@gmail.com>
2025-09-20 16:18:55 +0530

    20-Sep-25

--- src/AutomationFramework/Class2.java
+++ src/AutomationFramework/Class2.java
@@ -6,7 +6,7 @@ public class Class2 {
 	
 	public static void main(String[] args) {
 		
-		 int[] arr = {8, 5, 2, 4, 9, 6, 3, 1, 7};
+		   int[] arr = {8, 5, 2, 4, 9, 6, 3, 1, 7};
 	       int[] result = new int[arr.length];
 	       int index = 0;
 	        // Step 1: Copy evens in order
